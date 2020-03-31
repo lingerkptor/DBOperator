2bbc153e517e6fe55629b60f1189018b4c85bcdb
lingerkptor <lingerkptor@gmail.com>
2020-03-31 17:03:38 +0800

    Redesign DB config: add interface DatabaseConfig class

---
+++ src/main/java/idv/lingerkptor/util/DBOperator/DatabaseConfig.java
@@ -0,0 +1,10 @@
+package idv.lingerkptor.util.DBOperator;
+
+public interface DatabaseConfig {
+	public String getDriver();
+	public String getDriverUrl();
+	public String getUrl();
+	public String getAccount();
+	public String getPassword();
+	public int getMaxConnection();
+}
